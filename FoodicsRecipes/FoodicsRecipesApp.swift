d93a19d8a89122463df770aa5611ff7fa1810e38
amer266030 <amer266030@users.noreply.github.com>
2025-03-06 19:46:40 +0300

    initial commit

---
+++ FoodicsRecipes/FoodicsRecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodicsRecipesApp.swift
+//  FoodicsRecipes
+//
+//  Created by Amer Alyusuf on 06/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct FoodicsRecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FoodicsRecipesTests/FoodicsRecipesTests.swift
@@ -0,0 +1,17 @@
+//
+//  FoodicsRecipesTests.swift
+//  FoodicsRecipesTests
+//
+//  Created by Amer Alyusuf on 06/03/2025.
+//
+
+import Testing
+@testable import FoodicsRecipes
+
+struct FoodicsRecipesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
